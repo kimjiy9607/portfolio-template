2424c9567835ed5941f97810963c25c756befcc7
Leonid Sorkin <sorkinl@dickinson.edu>
2020-04-25 08:11:01 -0400

    Added terminal with a command to start portfolio, but CSS still needs tweaking

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Resume from './components/Resume';
@@ -7,10 +7,27 @@ import Testimonials from  './components/Testimonials';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+import Terminal  from 'terminal-in-react';
+import {render} from "react-dom";
+const App = () =>  {
+ 
+  const [showWebsite, setShowWebsite] = useState(false);
+  let returnValue;
+  if(showWebsite != true){
+    returnValue = (<div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "75px"
+      }}
+    >
+      <Terminal
+        commands={{'portfolio-start': () => setShowWebsite(true)}}
+      />
+    </div>)
+  } else {
+    returnValue = (<div className="App">
         <Header resumeData={resumeData}/>
         <About resumeData={resumeData}/>
         <Resume resumeData={resumeData}/>
@@ -18,9 +35,10 @@ class App extends Component {
         <Testimonials resumeData={resumeData}/>
         <ContactUs resumeData={resumeData}/>
         <Footer resumeData={resumeData}/>
-      </div>
-    );
+      </div>)
   }
-}
+    return returnValue;
+  }
+
 
 export default App;
